4366e5444305b6b60f00ea33f1a158ee342f180b
chiabauni <chiabauni@gmail.com>
2021-11-29 19:38:12 -0300

    Se ven los events

--- lib/tdl_elixir_web/controllers/users_controller.ex
+++ lib/tdl_elixir_web/controllers/users_controller.ex
@@ -13,7 +13,7 @@ defmodule TdlElixirWeb.UsersController do
   def show(conn, _params) do
     current_user = Pow.Plug.current_user(conn)
     user= current_user
-    |> Repo.preload(:tickets)
+    |> Repo.preload(tickets: [:event])
     Logger.info(user)
     render(conn, "show.html", user: user)
   end
