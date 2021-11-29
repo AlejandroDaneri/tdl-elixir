6c4926b88e9b7a71f02c0f25170f6f2d674be159
santiago.klein <santiago.klein@mulesoft.com>
2021-11-29 18:31:24 -0300

    Remove comments

--- lib/tdl_elixir_web/controllers/ticket_controller.ex
+++ lib/tdl_elixir_web/controllers/ticket_controller.ex
@@ -27,21 +27,5 @@ defmodule TdlElixirWeb.TicketController do
         |> put_flash(:error, "Error buying ticket.")
         Logger.error("Error buying ticket")
     end
-
-    # current_user
-    # |> Repo.preload(:events)
-    # |> Ecto.Changeset.change()
-    # |> Ecto.Changeset.put_assoc(:events, [Repo.get!(Event, event_id)])
-    # |> Repo.update()
-    # |> case do
-    #   {:ok, _} ->
-    #     conn
-    #     |> put_flash(:info, "the ticket was successfully bought.")
-    #     |> redirect(to: Routes.home_path(conn, :index))
-
-    #   {:error, _} ->
-    #     # handle error
-    #     Logger.error("Error buying ticket")
-    # end
   end
 end
